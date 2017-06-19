411ba2e21ad0c690c7d5ae8628226d4fa360a8ce
Nolan57 <urio@live.cn>
2017-06-19 23:11:19 +0800

    implement the CalculateRMSE function in tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,36 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+
+    // check the validity of the following inputs:
+    //  * the estimation vector size should not be zero
+    //  * the estimation vector size should equal ground truth vector size
+    if(estimations.size() != ground_truth.size()
+       || estimations.size() == 0){
+        cout << "Invalid estimation or ground_truth data" << endl;
+        return rmse;
+    }
+
+    //accumulate squared residuals
+    for(unsigned int i=0; i < estimations.size(); ++i){
+
+        VectorXd residual = estimations[i] - ground_truth[i];
+
+        //coefficient-wise multiplication
+        residual = residual.array()*residual.array();
+        rmse += residual;
+    }
+
+    //calculate the mean
+    rmse = rmse/estimations.size();
+
+    //calculate the squared root
+    rmse = rmse.array().sqrt();
+
+    //return the result
+    return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -36,7 +66,7 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
         cout << "CalculateJacobian () - Error - Division by Zero" << endl;
         return Hj;
     }
-    
+
     Hj <<  (px/c2), (py/c2), 0, 0,
           -(py/c1), (px/c1), 0, 0,
           py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
