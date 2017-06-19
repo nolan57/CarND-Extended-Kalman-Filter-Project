27e0e3cd88e4901105a4fd29725e801f519cee51
Nolan57 <urio@live.cn>
2017-06-19 23:07:24 +0800

    implement the CalculateJacobian function in tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -22,13 +22,32 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+    MatrixXd Hj (3, 4);
+    double px = x_state(0);
+    double py = x_state(1);
+    double vx = x_state(2);
+    double vy = x_state(3);
+
+    double c1 = px*px + py*py;
+    double c2 = sqrt(c1);
+    double c3 = c1*c2;
+
+    if(fabs(c1) < 0.0001){
+        cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+        return Hj;
+    }
+    
+    Hj <<  (px/c2), (py/c2), 0, 0,
+          -(py/c1), (px/c1), 0, 0,
+          py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+    return  Hj;
 }
 MatrixXd Tools::CalculateQ(const float ax2, const float ay2, const float deltaT) {
   /**
    * TODO
    * Calculate process noise Q here
    */
-    MatrixXd Q = MatrixXd(4 ,4);
+    MatrixXd Q(4 ,4);
     double deltaT2 = pow(deltaT, 2);
     double deltaT4 = deltaT2*deltaT2;
     double deltaT3 = deltaT2*deltaT;
