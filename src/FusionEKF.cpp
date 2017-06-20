9e783d61603ef92ce59beba18a28ec2e2659bf53
Nolan57 <urio@live.cn>
2017-06-20 23:12:31 +0800

    modify again on laser initialize state

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -113,7 +113,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       double vx = dpx / dt;
       double vy = dpy / dt;
       x_s << px, py, vx, vy;
-        ;
+        
       ekf_.H_ = H_laser_;
       ekf_.R_ = R_laser_;
       //ekf_.x_ = x_s;
