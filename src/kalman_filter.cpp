05faced4ed2bd2beb86d585cd8313d6f4496245a
Nolan57 <545266262@qq.com>
2017-06-20 13:29:40 +0800

    Update kalman_filter.cpp
    implement the UpdateEKF function

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -46,4 +46,11 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  VectorXd y = z - H_ * x_;
+  MatrixXd S = H_ * P_ * H_.transpose() + R_ ;
+  MatrixXd K = P_ * H_ * S.inverse();
+  x_ = x_ + K * y;
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_ ;
 }
