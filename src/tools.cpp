570c901bd743655175b381ef7d50fca56bf5b689
Nolan57 <545266262@qq.com>
2017-09-17 12:01:43 +0800

    Update tools.cpp
    correct the change in ignoring first 10 data in calculation of RMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -28,8 +28,11 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     }
 
     //accumulate squared residuals
-    for(unsigned int i=10; i < estimations.size(); ++i){
-
+    for(unsigned int i=0; i < estimations.size(); ++i){
+      //ignore the first 10 data
+        if(i < 10){
+          continue;
+          )
         VectorXd residual = estimations[i] - ground_truth[i];
 
         //if ((residual.maxCoeff() > 5) | (residual.minCoeff() < -5)){
